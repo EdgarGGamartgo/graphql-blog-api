e939b7fedddfcc53174c324a97f85dac8485474d
EdgarGGamartgo <43557626+EdgarGGamartgo@users.noreply.github.com>
2020-09-07 00:34:28 -0500

    Subscription setup

--- src/db.js
+++ src/db.js
@@ -1,90 +1,59 @@
-const posts = [
-    {
-        id: '1',
-        title: 'My first post',
-        body: 'Dont do lazy things',
-        published: false,
-        author: "1"
-    },
-    {
-        id: '2',
-        title: 'My second post',
-        body: 'Dont do lazy things',
-        published: true,
-        author: "1"
-    },
-    {
-        id: '3',
-        title: 'My third post',
-        body: 'Dont do lazy things',
-        published: false,
-        author: "2"
-    },
-    {
-        id: '4',
-        title: 'My fourth post',
-        body: 'Dont do lazy things',
-        published: true,
-        author: "3"
-    }
-]
+const users = [{
+    id: '1',
+    name: 'Edgar',
+    email: 'edgar@example.com',
+    age: 24
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Andrea',
+    email: 'andrea@example.com'
+}]
+
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: true,
+    author: '2'
+}]
 
 const comments = [{
-    id: "1",
-    text: "This is my first comment ...",
-    author: "1",
-    post: "1"
-},
-{
-    id: "2",
-    text: "This is my second comment ...",
-    author: "2",
-    post: "2"
-},
-{
-    id: "3",
-    text: "This is my third comment ...",
-    author: "3",
-    post: "3"
-},
-{
-    id: "4",
-    text: "This is my fourth comment ...",
-    author: "4",
-    post: "4"
-},
-{
-    id: "5",
-    text: "This is my fifth comment ...",
-    author: "2",
-    post: "1"
+    id: '102',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '12'
 }]
-
-const users = [{
-    id: "1",
-    name: "Edgar",
-    email: "edgar@gmail.com",
-    age: 24
-},
-{
-    id: "2",
-    name: "Gabriel",
-    email: "gabriel@gmail.com",
-    age: 47
-},
-{
-    id: "3",
-    name: "David",
-    email: "david@gmail.com",
-    age: 19
-},
-{
-    id: "4",
-    name: "Rosario",
-    email: "rosario@gmail.com",
-    age: 70
-}]
-
 
 const db = {
     users,
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,40 +1,41 @@
 const Query = {
     users(parent, args, { db }, info) {
-
         if (!args.query) {
             return db.users
         }
 
-        return db.users.filter(user => {
+        return db.users.filter((user) => {
             return user.name.toLowerCase().includes(args.query.toLowerCase())
         })
-    },
-    comments() {
-        return comments
-    },
-    me() {
-        return {
-            id: '123098',
-            name: 'Gabriel',
-            email: 'example@gamil.com',
-            age: 20
-        }
-    },
-    post() {
-        return {
-            id: '12345',
-            title: 'My first post',
-            body: 'Dont do lazy things',
-            published: false
-        }
     },
     posts(parent, args, { db }, info) {
         if (!args.query) {
             return db.posts
         }
+
         return db.posts.filter((post) => {
-            return post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())
+            const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+            const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+            return isTitleMatch || isBodyMatch
         })
+    },
+    comments(parent, args, { db }, info) {
+        return db.comments
+    },
+    me() {
+        return {
+            id: '123098',
+            name: 'Mike',
+            email: 'mike@example.com'
+        }
+    },
+    post() {
+        return {
+            id: '092',
+            title: 'GraphQL 101',
+            body: '',
+            published: false
+        }
     }
 }
 
